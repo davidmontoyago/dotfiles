92088d29b623f4720fc5825aedd78f1a095a0f52
David Montoya <davidmontoyago@gmail.com>
2019-10-12 17:13:05 -0500

    extract functions

--- install.sh
+++ install.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -u -o pipefail
-
-function install_dotfile() {
-  DOTFILE=$1
-  SRCDIR=${2:-./}
-  DESTDIR=${3:-$HOME}
-  STAMP=$(date '+%Y-%m-%d-%H%M%S')
-  if [ -e $DESTDIR/$DOTFILE ]; then
-    echo "backing up previous $DOTFILE..."
-    mv $DESTDIR/$DOTFILE "$DESTDIR/$DOTFILE-backup-$STAMP"
-  fi
-
-  cp $SRCDIR/$DOTFILE $DESTDIR
-  echo "installed $DESTDIR/$DOTFILE"
-}
+source ./functions.sh
 
 install_dotfile "Brewfile" "homebrew"
 bash ./homebrew/install.sh
