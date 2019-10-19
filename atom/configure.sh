61f25e995d689f4526a5a340584280f799979044
David Montoya <davidmontoyago@gmail.com>
2019-10-19 09:58:31 -0500

    install only compatible atom packages

--- atom/configure.sh
+++ atom/configure.sh
@@ -3,6 +3,6 @@ set +u
 
 printf "\nsetting up atom...\n"
 
-apm install --packages-file ./atom/package-list.txt
+apm install --compatible --packages-file ./atom/package-list.txt
 
 #TODO pip install python-language-server
