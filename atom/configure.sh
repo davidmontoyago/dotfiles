108ec58b4cf150dd272a190a00e086777f870024
David Montoya <davidmontoyago@gmail.com>
2019-11-08 21:58:57 -0600

    skip installing of already bundled atom packages

--- atom/configure.sh
+++ atom/configure.sh
@@ -3,6 +3,11 @@ set +u
 
 printf "\nsetting up atom...\n"
 
-apm install --compatible --packages-file ./atom/package-list.txt
+# install only non-core atom packages
+# core packages are already bundled with Atom and re-installing makes Atom unstable
+tmppkgsfile=$(mktemp)
+grep -vf ./atom/skip-packages.txt ./atom/package-list.txt > $tmppkgsfile
+apm install --compatible --packages-file $tmppkgsfile
+rm $tmppkgsfile
 
 #TODO pip install python-language-server
