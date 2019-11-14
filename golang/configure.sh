0403713796968aa37ff819fe1aa5cadfb5daf13a
David Montoya <davidmontoyago@gmail.com>
2019-11-14 17:18:32 -0600

    install go delve

--- golang/configure.sh
+++ golang/configure.sh
@@ -5,5 +5,8 @@ GOWORKSPACE="$HOME/projects/go/src/github.com/davidmontoyago"
 test -d "$GOWORKSPACE" || mkdir -p "$GOWORKSPACE"
 test -d "$HOME/bin" || mkdir -p "$HOME/bin"
 
+# delve debugger
+go get -u github.com/go-delve/delve/cmd/dlv
+
 # protoc go plugin
 go get -u github.com/golang/protobuf/protoc-gen-go
