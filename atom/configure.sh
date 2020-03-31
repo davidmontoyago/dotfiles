58e5552f0542a92e0b0b6d10c5c9bd599a365ac3
David Montoya <davidmontoyago@gmail.com>
2020-03-30 19:53:12 -0500

    fix atom path

--- atom/configure.sh
+++ atom/configure.sh
@@ -4,7 +4,7 @@ source ./functions.sh
 
 printf "\nsetting up atom...\n"
 
-install_dotfile "config.cson" "atom" "$HOME/.atom"
+install_dotfile "config.cson" "atom" "$HOME/.atom/"
 
 # install only non-core atom packages
 # core packages are already bundled with Atom and re-installing makes Atom unstable
